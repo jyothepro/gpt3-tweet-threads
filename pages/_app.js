fe074b0581b8ac9216bdea3ced2acb8856d479d0
jyo <cooljyo@gmail.com>
2022-12-05 21:45:43 -0800

    adding google analytics and updated html meta data

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,24 @@
 import './styles.css';
+import Script from 'next/script';
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-VTRQPTPQYP"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-VTRQPTPQYP');
+        `}
+      </Script>
+      <Component {...pageProps} />
+      </>
+  );
 }
 export default App;
--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Tweet Thread Generator" key="title"/>
+        <meta property="og:description" content="Make yoru tweets go viral!" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://media.discordapp.net/attachments/1046841725127250000/1049556132122873926/Screenshot_2022-12-05_at_9.20.40_PM.png?width=810&height=696"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
