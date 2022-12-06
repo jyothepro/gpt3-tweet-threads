ae5305c51f6af66244e0a8ebca9bd09803e7bfd2
jyo <cooljyo@gmail.com>
2022-12-05 20:31:46 -0800

    update color and logi

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import appLogo from '../assets/app-logo.png';
 import { useState } from 'react';
 
 
@@ -77,13 +77,13 @@ const Home = () => {
       </div>
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-writer"
+          href="https://twitter.com/jyothepro"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <Image src={appLogo} alt="app logo" />
+            <p>built by @jyothepro</p>
           </div>
         </a>
       </div>
